9429a8489dbc8d0711d48009c3baaf6ff3241957
MartyLake <matthieu@talbot.audio>
2020-03-28 20:12:17 +0100

    Add current_playing_song.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mpc_utils
-various mpc utilities
+* various mpc utilities
+* uses https://pypi.org/project/python-mpd2/
+* just call script to see position of arguments
+
+## current_playing_song.py
+This script fetches the current mpd playing song in the specified file. You can then source this file in OBS studio for example in your "be right back" or "starting soon" screen.
---
+++ current_playing_song.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import mpd
+import sys
+
+if len(sys.argv) < 3 :
+    print ("usage: {} [FILEOUT PATH] [URL] [PORT] [PASSWORD]".format(sys.argv[0]))
+    print ("then, you can source this file in OBS studio in your BRB screen of Starting soon screen.")
+    sys.exit(1)
+
+FILEOUT=sys.argv[1]
+URL=sys.argv[2]
+PORT=sys.argv[3]
+PASSWORD=None
+if len(sys.argv) >= 4:
+    PASSWORD=sys.argv[4]
+
+# use_unicode will enable the utf-8 mode for python2
+# see https://python-mpd2.readthedocs.io/en/latest/topics/advanced.html#unicode-handling
+client = mpd.MPDClient(use_unicode=True)
+client.connect(URL, 6600)
+if PASSWORD:
+    client.password(PASSWORD)
+
+while True:
+    status = client.status()
+    #for x in status:
+    #    print(x, status[x])
+    if not status["state"] == "play":
+        print("Not playing.")
+        with open(FILEOUT, "w") as f:
+            f.write("Now playing: 4'33\" - John Cage")
+    else:
+        song = client.currentsong()
+        #for x in song:
+        #    print(x, song[x])
+        outstr = "Now playing: {} - {}".format(song["title"], song["artist"])
+        print(outstr)
+        with open(FILEOUT, "w") as f:
+             f.write(outstr)
+
+    client.idle()
